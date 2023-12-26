7cc805ca9f4d9e35fa5cd1752ac92681cc304d32
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-26 16:54:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Wait for the specified duration and then redirect to home.html
     setTimeout(function () {
-        window.location.href = 'antara-web/home/';
+        window.location.href = '/home/';
     }, splashDuration);
 
     // Add event listener for hover effect on the text
