0fbb7b288ee1cfaf04a87f923fca11711ce03aaf
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-27 18:10:43 +0530

    Update script.js

--- home/script.js
+++ home/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             axios.get(`https://youtube-music-api3.p.rapidapi.com/top?country=${userCountry}`, {
                 headers: {
-                    'X-RapidAPI-Key': 'your api key',
+                    'X-RapidAPI-Key': '54fb139661mshb7ee757010901c9p177c1ajsn4a5b60261d0d',
                     'X-RapidAPI-Host': 'youtube-music-api3.p.rapidapi.com'
                 }
             })
